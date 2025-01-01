64b9a93bfa81e0bc244c9bb6547607840e71ac10
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2025-01-01 18:09:35 +0100

    Almost done.
    Added more JS and everything seams to be ok.

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ function appendToDisplay(input) {
 }
 
 function clearDisplay() {
-
+    display.value = "";
 }
 
 function calculate() {
-
+    try {
+        display.value = eval(display.value);
+    } catch(error) {
+        display.value = "Error";
+    }
 }
